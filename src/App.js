47ca9fbeb6efff0c81d491634fbde005f18e2d70
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-10-18 14:38:37 +0900

    feat(App): 문자열 계산기 메인 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Validator from './domain/Validator.js';
+import Separator from './domain/Separator.js';
+import AdditionCalculator from './domain/AdditionCalculator.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+
+    if (input.startsWith('//')) {
+      Validator.validateFormatManual(input);
+    }
+    const delimiters = Separator.getDelimiters(input);
+    Validator.validateCharactersManual(input, delimiters);
+
+    const calculator = new AdditionCalculator(delimiters);
+    const strNum = calculator.split(input);
+
+    const numbers = strNum.map(Number);
+    Validator.validateNumbers(numbers);
+
+    const result = calculator.sum(numbers);
+    MissionUtils.Console.print(`결과: ${result}`);
+  }
 }
 
 export default App;
