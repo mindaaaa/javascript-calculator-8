45f4368ad1626fb904c2142463766cc0f53b7d0d
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-10-18 13:38:32 +0900

    feat(Separator): getDelimiters 구현

--- src/domain/Separator.js
+++ src/domain/Separator.js
@@ -8,6 +8,21 @@ class Separator {
     const newlineInput = input.indexOf('\n');
     return input.slice(2, newlineInput);
   }
+
+  /**
+   * 모든 구분자 반환 (기본 & 커스텀)
+   * @param {string} input - 입력 문자열
+   * @returns {string[]} 구분자 배열
+   */
+  static getDelimiters(input) {
+    const defaultDelimiters = [',', ':'];
+    const customDelimiter = this.extractCustomDelimiter(input);
+
+    if (customDelimiter) {
+      return [...defaultDelimiters, customDelimiter];
+    }
+    return defaultDelimiters;
+  }
 }
 
 export default Separator;
