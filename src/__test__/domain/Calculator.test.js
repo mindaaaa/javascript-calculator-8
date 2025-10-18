c192b23cc3a618c46bcc72da97018271da68727b
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-10-18 13:59:14 +0900

    test(Calculator): split 테스트 작성

---
+++ src/__test__/domain/Calculator.test.js
@@ -0,0 +1,41 @@
+import Calculator from '../../domain/Calculator.js';
+
+describe('Calculator 클래스', () => {
+  describe('split', () => {
+    test('기본 구분자로 문자열을 분리한다', () => {
+      // given
+      const calculator = new Calculator([',', ':']);
+      const input = '1,2:3';
+
+      // when
+      const result = calculator.split(input);
+
+      // then
+      expect(result).toEqual(['1', '2', '3']);
+    });
+
+    test('커스텀 구분자로 문자열을 분리한다', () => {
+      // given
+      const calculator = new Calculator([',', ':', ';']);
+      const input = '//;\n1;2;3';
+
+      // when
+      const result = calculator.split(input);
+
+      // then
+      expect(result).toEqual(['1', '2', '3']);
+    });
+
+    test('빈 문자열은 빈 배열을 반환한다', () => {
+      // given
+      const calculator = new Calculator([',', ':']);
+      const input = '';
+
+      // when
+      const result = calculator.split(input);
+
+      // then
+      expect(result).toEqual([]);
+    });
+  });
+});
