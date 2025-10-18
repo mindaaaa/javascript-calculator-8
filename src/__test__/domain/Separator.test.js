c779bb3fe00b720d70ae177c456a37ecaa77a79f
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-10-18 13:38:50 +0900

    test(Separator): getDelimiters 테스트 작성

--- src/__test__/domain/Separator.test.js
+++ src/__test__/domain/Separator.test.js
@@ -35,4 +35,50 @@ describe('Separator 클래스', () => {
       expect(result).toBe('*');
     });
   });
+
+  describe('getDelimiters', () => {
+    test('커스텀 구분자가 있으면 기본 구분자와 함께 반환한다', () => {
+      // given - 이미 검증된 입력
+      const input = '//;\n1;2;3';
+
+      // when
+      const result = Separator.getDelimiters(input);
+
+      // then
+      expect(result).toEqual([',', ':', ';']);
+    });
+
+    test('커스텀 구분자가 없으면 기본 구분자만 반환한다', () => {
+      // given
+      const input = '1,2:3';
+
+      // when
+      const result = Separator.getDelimiters(input);
+
+      // then
+      expect(result).toEqual([',', ':']);
+    });
+
+    test('여러 글자 커스텀 구분자도 배열에 포함한다', () => {
+      // given
+      const input = '//abc\n1abc2';
+
+      // when
+      const result = Separator.getDelimiters(input);
+
+      // then
+      expect(result).toEqual([',', ':', 'abc']);
+    });
+
+    test('빈 문자열은 기본 구분자만 반환한다', () => {
+      // given
+      const input = '';
+
+      // when
+      const result = Separator.getDelimiters(input);
+
+      // then
+      expect(result).toEqual([',', ':']);
+    });
+  });
 });
